03354b97bd00d59a2f40ab8fc2e83d5b3ba8ef6d
Miguel <miguelopezrodriguez96@gmail.com>
2023-10-19 12:24:35 +0200

    Esto es una prueba que se está realizando para que funcione todo bien

---
+++ app/src/main/java/com/example/a2dammoviles/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.a2dammoviles
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a2dammoviles.ui.theme._2DAMMovilesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            _2DAMMovilesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _2DAMMovilesTheme {
+        Greeting("Android")
+    }
+}
